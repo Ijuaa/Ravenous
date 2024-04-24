a08922f3a1ebeccaa46a80407b7e051a2a2d607e
Ijuaa <pierre.colpart@gmail.com>
2024-04-24 16:18:29 +0200

    ravenous part 1

---
+++ src/Components/SearchBar.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+import '../styles/SearchBar.scss';
+
+const sortOptions = {
+    'Best Match': 'best_match',
+    'Highest Rated': 'rating',
+    'Most Reviewed': 'review_count'
+};
+
+function SearchBar() {
+    const [term, setTerm] = useState('');
+    const [location, setLocation] = useState('');
+    const [sortBy, setSortBy] = useState('best_match');
+
+    const handleTermChange = event => {
+        setTerm(event.target.value);
+    };
+
+    const handleLocationChange = event => {
+        setLocation(event.target.value);
+    };
+
+    const handleSortByChange = sortOption => {
+        setSortBy(sortOption);
+    };
+
+    const handleSearch = event => {
+        console.log(`Searching for ${term} in ${location} sorted by ${sortBy}`);
+        // Here you would typically handle the actual search API request.
+        event.preventDefault();
+    };
+
+    return (
+        <div className="search-bar">
+            <ul className="sort-options">
+                {Object.keys(sortOptions).map(option => (
+                    <li 
+                        key={option} 
+                        className={sortBy === sortOptions[option] ? 'active' : ''}
+                        onClick={() => handleSortByChange(sortOptions[option])}
+                    >
+                        {option}
+                    </li>
+                ))}
+            </ul>
+            <div className='line'></div>
+            <form onSubmit={handleSearch}>
+                <div>
+                <input 
+                    placeholder="Search Businesses" 
+                    value={term} 
+                    onChange={handleTermChange} 
+                />
+                <input 
+                    placeholder="Where?" 
+                    value={location} 
+                    onChange={handleLocationChange} 
+                />
+                </div>
+                <button type="submit">Let's go</button>
+            </form>
+        </div>
+    );
+}
+
+export default SearchBar;
