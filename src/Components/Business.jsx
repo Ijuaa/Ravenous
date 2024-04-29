ce72cc0787dbf063d0172730fb5bb81683c8d525
Ijuaa <pierre.colpart@gmail.com>
2024-04-29 18:02:21 +0200

    starting E3

--- src/Components/Business.jsx
+++ src/Components/Business.jsx
@@ -1,31 +1,46 @@
-import { businessData as bus } from "../data/business";
+import PropTypes from "prop-types";
 import "../styles/Business.scss";
 
-function Business() {
+function Business({ business }) {
   return (
     <div className="business_container">
       <div className="business_image">
-        <img src={bus.imageSrc} alt={bus.name} />
-        <h2 className="business_name">{bus.name}</h2>
+        <img src={business.imageSrc} alt={business.name} />
+        <h2 className="business_name">{business.name}</h2>
       </div>
       <div className="business_infos">
         <div className="business_adress">
-          <p>{bus.address}</p>
-          <p>{bus.city}</p>
+          <p>{business.address}</p>
+          <p>{business.city}</p>
           <p>
-            {bus.state} {bus.zipCode}
+            {business.state} {business.zipCode}
           </p>
         </div>
         <div className="business_reviews">
-          <h3>{bus.category}</h3>
-          <h3>{bus.rating} stars</h3>
+          <h3>{business.category}</h3>
+          <h3>{business.rating} stars</h3>
           <p>
-            {bus.reviewCount} {bus.reviewCount > 1 ? "reviews" : "review"}
+            {business.reviewCount}{" "}
+            {business.reviewCount > 1 ? "reviews" : "review"}
           </p>
         </div>
       </div>
     </div>
   );
 }
+
+Business.propTypes = {
+  business: PropTypes.shape({
+    imageSrc: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    address: PropTypes.string.isRequired,
+    city: PropTypes.string.isRequired,
+    state: PropTypes.string.isRequired,
+    zipCode: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    reviewCount: PropTypes.number.isRequired,
+  }).isRequired,
+};
 
 export default Business;
--- src/Components/BusinessList.jsx
+++ src/Components/BusinessList.jsx
@@ -1,16 +1,19 @@
+import PropTypes from "prop-types";
 import Business from "./Business";
 import "../styles/BusinessList.scss";
-function BusinessList() {
+
+function BusinessList({ businesses }) {
   return (
     <div className="business_list_container">
-      <Business />
-      <Business />
-      <Business />
-      <Business />
-      <Business />
-      <Business />
+      {businesses.map((business, index) => (
+        <Business key={index} business={business} />
+      ))}
     </div>
   );
 }
+
+BusinessList.propTypes = {
+  businesses: PropTypes.array.isRequired,
+};
 
 export default BusinessList;
